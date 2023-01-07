23e019b312e55eb570e629a52d64a061139b1aab
Balaji Dontul <83111706+balajidontul@users.noreply.github.com>
2023-01-07 20:58:34 +0530

    added main thread example

---
+++ CurrentThreadDemo.java
@@ -0,0 +1,22 @@
+package Threads;
+
+//controlling the main thread
+public class CurrentThreadDemo {
+    public static void main(String[] args) {
+
+        //get current thread data
+        Thread t = Thread.currentThread();
+        System.out.println( "Current thread :" + t);
+
+        t.setName("My Thread");
+        System.out.println("After name Change :" + t);
+
+        try {
+            Thread.sleep(1000); //1 Second
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    
+    }
+}
